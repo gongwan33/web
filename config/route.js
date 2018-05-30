6972d42e4c697e971561c0feee3fb23f677c19ee
gongwan33 <gongwan33@gmail.com>
2018-05-30 11:58:34 +0800

    v0.9

--- config/route.js
+++ config/route.js
@@ -10,6 +10,11 @@ app.get(getLocaleStylePath('/'), (req, res) => {
     res.render('index', {pageLocale: locale});
 });
 
+app.get(getLocaleStylePath('/scrum-training'), (req, res) => {
+    let locale = getLocale();
+    res.render('pages/subpage', {pageLocale: locale, contentTag: 'scrumtraining'});
+});
+
 app.get(getLocaleStylePath('/network-service'), (req, res) => {
     let locale = getLocale();
     res.render('pages/subpage', {pageLocale: locale, contentTag: 'networkservice'});
@@ -19,6 +24,17 @@ app.get(getLocaleStylePath('/miscellaneous'), (req, res) => {
     let locale = getLocale();
     res.render('pages/subpage', {pageLocale: locale, contentTag: 'miscellaneous'});
 });
+
+app.get(getLocaleStylePath('/dorry-platform'), (req, res) => {
+    let locale = getLocale();
+    res.render('pages/subpage', {pageLocale: locale, contentTag: 'dorryplatform'});
+});
+
+app.get(getLocaleStylePath('/hiring'), (req, res) => {
+    let locale = getLocale();
+    res.render('pages/subpage', {pageLocale: locale, contentTag: 'hiring'});
+});
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
