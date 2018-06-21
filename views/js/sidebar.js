d2fa13b1e258a259e4507d2f02e3dddb7b5b0a63
gongwan33 <gongwan33@gmail.com>
2018-06-21 15:38:24 +0800

    add persist side bar

--- views/js/sidebar.js
+++ views/js/sidebar.js
@@ -1,12 +1,26 @@
-import {MDCTemporaryDrawer} from '@material/drawer';
+import {MDCTemporaryDrawer, MDCPersistentDrawer} from '@material/drawer';
 
-let drawer = new MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
+const temDrawerEle = document.querySelector('.mdc-drawer--temporary');
+const perDrawerEle = document.querySelector('.mdc-drawer--persistent');
+const drawer = new MDCTemporaryDrawer(temDrawerEle);
+const pdrawer = new MDCPersistentDrawer(perDrawerEle);
+
+const breakpoint = 1024;
 
 document.querySelector('.menu').addEventListener('click', () => {
-    if(drawer.open) {
-        drawer.open = false;
+    let width = document.body.clientWidth;
+    if(width < breakpoint) {
+        if(drawer.open) {
+            drawer.open = false;
+        } else {
+            drawer.open = true;
+        }
     } else {
-        drawer.open = true;
+        if(pdrawer.open) {
+            pdrawer.open = false;
+        } else {
+            pdrawer.open = true;
+        }
     }
 });
 
@@ -15,14 +29,36 @@ window.onresize = function(event) {
     if(oldEvent) {
         oldEvent(event);
     }
-
-    let menuDrawerContent = document.querySelector('.side-bar-drawerable');
-    let menuDrawer = document.querySelector('.mdc-drawer');
-    let menuDCntDisplay = menuDrawerContent.style.display ? menuDrawerContent.style.display : getComputedStyle(menuDrawerContent, null).display;
-
-    if(menuDCntDisplay == 'none') {
-        menuDrawer.style.display = 'none';
+    
+    let width = document.body.clientWidth;
+    if(width < 1024) {
+        perDrawerEle.style.display = 'none';
+        temDrawerEle.style.display = '';
+        pdrawer.open = false;
     } else {
-        menuDrawer.style.display = '';
+        temDrawerEle.style.display = 'none';
+        perDrawerEle.style.display = '';
+        pdrawer.open = true;
+        drawer.open = false;
     }
 }
+
+oldEvent = window.onload;
+window.onload = function() {
+    if(oldEvent) {
+        oldEvent(event);
+    }
+
+    let width = document.body.clientWidth;
+    if(width < 1024) {
+        perDrawerEle.style.display = 'none';
+        pdrawer.open = false;
+        temDrawerEle.style.display = '';
+        drawer.open = false;
+    } else {
+        temDrawerEle.style.display = 'none';
+        drawer.open = false;
+        perDrawerEle.style.display = '';
+        pdrawer.open = true;
+    }
+} 
